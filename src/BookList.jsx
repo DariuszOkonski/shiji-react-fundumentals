88bab16507099210ba56951828b2f5ed4e31d27c
Dariusz Okoński <darek200180@gmail.com>
2024-04-03 19:52:33 +0200

    add EventExamples

--- src/BookList.jsx
+++ src/BookList.jsx
@@ -2,31 +2,35 @@ const title = 'Onyx Storm';
 const author = 'Rebecca Yarros';
 const img = './images/book1.jpg';
 
-const firstBook = {
-  author: 'Jordan More',
-  title: 'Interesting Facts For Curious Minds',
-  img: './images/book1.jpg',
-};
-
-const secondBook = {
-  author: 'James Clear',
-  title: 'Atomic Habits',
-  img: './images/book2.jpg',
-};
+const books = [
+  {
+    id: 1,
+    author: 'Jordan More',
+    title: 'Interesting Facts For Curious Minds',
+    img: './images/book1.jpg',
+  },
+  {
+    id: 2,
+    author: 'Jordan More',
+    title: 'Interesting Facts For Curious Minds',
+    img: './images/book2.jpg',
+  },
+  {
+    id: 3,
+    author: 'Johnny Bravo',
+    title: 'Something stupid',
+    img: './images/book3.jpg',
+  },
+];
 
 export const BookList = () => {
   return (
     <section className='booklist'>
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      <EventExamples />
+
+      {books.map((book) => (
+        <Book key={book.id} {...book} />
+      ))}
     </section>
   );
 };
@@ -38,5 +42,32 @@ const Book = ({ img, title, author }) => {
       <h2>{title}</h2>
       <h4>{author.toUpperCase()}</h4>
     </article>
+  );
+};
+
+const EventExamples = () => {
+  const handleFormInput = (e) => {
+    e.preventDefault();
+    console.log('hello');
+  };
+
+  const handleButtonClick = (e) => {
+    e.preventDefault();
+    console.log('click');
+  };
+
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type='text'
+          name='example'
+          style={{ margin: '1rem 0' }}
+          onChange={handleFormInput}
+        />
+        <button onClick={handleButtonClick}>click me</button>
+      </form>
+    </section>
   );
 };
